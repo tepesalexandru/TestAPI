b5befe718d8721a29f290f5cc3fddd61c782bd75
Alexandru Tepes <Alexandru.Tepes@qubiz.com>
2022-04-06 09:30:26 +0300

    product class

---
+++ TestAPI/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestAPI.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int[] Ratings { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}

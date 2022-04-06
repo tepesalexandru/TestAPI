b8fa4e23bf2fcb63197aea8ec097001637f808af
Alexandru Tepes <Alexandru.Tepes@qubiz.com>
2022-04-06 10:26:09 +0300

    product manager create / get all

--- TestAPI/Business/ProductManager.cs
+++ TestAPI/Business/ProductManager.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections.Generic;
 using TestAPI.Models;
+using TestAPI.Repositories;
 
 namespace TestAPI.Business
 {
     public interface IProductManager
     {
         List<Product> GetProducts();
+        Product CreateProduct(Product product);
     }
 
     public class ProductManager : IProductManager
     {
+        private readonly IProductRepository _productRepository;
+        public ProductManager(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public Product CreateProduct(Product product)
+        {
+            return _productRepository.CreateProduct(product);
+        }
+
         public List<Product> GetProducts()
         {
-            return null;
+            return _productRepository.GetProducts();
         }
     }
 
